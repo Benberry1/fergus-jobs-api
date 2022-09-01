8366ce8bea5ce679a8bacac01ab0415b683a5065
Ben Berry <ben.berry@developersinstitute.co.nz>
2022-09-02 11:48:56 +1200

    audit: adjusted jobs.sql insert data for dates to make more sense

--- db/sql/03_jobs.sql
+++ db/sql/03_jobs.sql
@@ -1,5 +1,5 @@
 INSERT INTO jobs (id, status, date_created, customer_id, notes) VALUES
-(1, 'completed', '2022-06-14 23:59:21', 1, ARRAY ['Job complete 2nd June 22']),
+(1, 'completed', '2022-06-14 23:59:21', 1, ARRAY ['Job complete 22nd June 22']),
 (2, 'completed', '2022-06-24 23:59:21', 2, ARRAY ['Completed job 01/07/22']),
 (3, 'invoicing', '2022-07-12 23:59:21', 3, ARRAY ['Called customer and set job for 20th July', 'Customer called and rescheduled to 1st August', 'Job completed 20th August']),
 (4, 'active', '2022-07-19 23:59:21', 1, ARRAY ['Call customer to organise a time to meet and price', 'Job schedule for 1st September']),
